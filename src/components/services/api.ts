681599ad687e779a0cb14c6d048f4953ee9041e3
Dovhan_Eduard <dovhan.eduard@gmail.com>
2024-09-20 00:39:47 +0300

    Update api.ts

--- src/components/services/api.ts
+++ src/components/services/api.ts
@@ -1,10 +1,16 @@
 import axios from 'axios';
+import { Image } from '../ImageGallery/ImageGallery.types';
+
+type ResponseData = {
+  results: Image[];
+  total_pages: number;
+};
 
 export const getImagesByValue = async (
   page: number,
   searchValue: string
-): Promise<Response> => {
-  const { data } = await axios.get<Response>(
+): Promise<ResponseData> => {
+  const { data } = await axios.get<ResponseData>(
     `https://api.unsplash.com/search/photos?client_id=OENp_TSYFm3eWIima9PMumghXbRwh14LV4MW7SQ5VnY&query=${searchValue}&page=${page}`
   );
 
